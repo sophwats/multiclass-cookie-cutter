8a5c68fd6bdc6c2fd07d4a112fac264fc7d06081
Sophie Watson <sophwats@gmail.com>
2020-07-13 13:58:21 -0500

    re-added requirements

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
+import click
 import logging
+from pathlib import Path
 import pandas as pd
+from dotenv import find_dotenv, load_dotenv
 import os
 
 
-
+@click.command()	
+@click.argument('input_filepath', type=click.Path(exists=True))	
+@click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
